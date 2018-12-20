562d23e7c01cd316cfc9da5a82809d4c8b57a6a0
07am <45973472+07am@users.noreply.github.com>
2018-12-20 18:11:08 +0000

    Initial upload of scan scripts

---
+++ tParser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# tParser v1.0
+# Utility designed to parse current directory for GNMAP files for open ports.
+# tParser.sh
+# author 07am [https://github.com/07am]
+
+while true; do
+clear
+date=$(date)
+ro=$(grep -h "open\|closed" *.gnmap)
+if [[ $ro == *"open"* || $ro == *"closed"* ]]
+then
+	for ((i=10; i>=1; i--));do
+		printf "\nUpdated $date\n\nNon-Filtered Ports Found:\n\n$ro\n\nChecking again in $i seconds...\n"
+		sleep 1
+		clear
+	done
+else
+	for ((i=10; i>=1; i--));do
+		printf "\nUpdated $date\n\nNo results yet.\n\nChecking again in $i seconds...\n"
+		sleep 1
+		clear
+	done
+fi
+done
+
